2069f5fd0730bcb26eaa62f63087e36e204adc31
mayanhavoc <mayanhavoc@gmail.com>
2021-09-01 19:23:09 -0700

    finished tutorial

--- src/App.js
+++ src/App.js
@@ -1,25 +1,103 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import { useGoogleReCaptcha } from "react-google-recaptcha-v3";
+import Grid from "@material-ui/core/Grid";
+import TextField from "@material-ui/core/TextField";
 
-function App() {
+const App = () => {
+  const [data, setData] = useState(null);
+  const { executeRecaptcha } = useGoogleReCaptcha();
+  const [headerInfo, setHeaderInfo] = useState("");
+
+  const handleSearch = async (event) => {
+    const token = await executeRecaptcha("submit");
+    axios
+      .get(
+        `https://chova32n.korconnect.io/GIPHY/v1/channels/search?q=${event.target.value}`,
+        {
+          headers: {
+            token,
+            "api_key": "jlSE9hwj237w4SczihD2049pJACJu6HF8CNH4z5R",
+          },
+        }
+      )
+      .then((response) => {
+        setData(response.data.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+  // Create an event handler so you can call the verification on button click event or form submit
+  const handleGet = async () => {
+    if (!executeRecaptcha) {
+      console.log("Execute recaptcha not yet available");
+    }
+    const token = await executeRecaptcha("submit");
+    
+    // Do whatever you want with the token
+    axios
+      .get("https://chova32n.korconnect.io/GIPHY/v1/gifs/random", {
+        headers: {
+          token,
+          "api_key": "jlSE9hwj237w4SczihD2049pJACJu6HF8CNH4z5R",
+        },
+      })
+      .then((response) => {
+        setHeaderInfo(response.data.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+  // You can use useEffect to trigger the verification as soon as the component being loaded
+    useEffect(() => {
+      if (executeRecaptcha) {
+        handleGet();
+      }
+       // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [executeRecaptcha]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Grid xs={12} item={true}>
+        <h1 className="center-align">Gif Explorer</h1>
+      </Grid>
+      <Grid
+        xs={12}
+        item={true}
+        container
+        direction="row"
+        justifyContent="center"
+        alignItems="center"
+      >
+        <img src={headerInfo.image_url} alt="logo" />
+      </Grid>
+      <Grid xs={12} item={true}>
+        <TextField
+          id="standard-basic"
+          label="Search"
+          fullWidth
+          onChange={(e) => handleSearch(e)}
+        />
+      </Grid>
+      {data && data.map(
+        (gif) =>
+          gif.banner_image && (
+            <Grid
+              xs={12}
+              item={true}
+              md={3}
+              key={data.id}
+              container="row"
+              justifyContent="center"
+              alignItems="center"
+            >
+              <img src={gif.banner_image} alt="banner" />
+            </Grid>
+          )
+      )}
+    </>
   );
-}
-
+};
 export default App;
--- src/index.js
+++ src/index.js
@@ -2,16 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { GoogleReCaptchaProvider } from 'react-google-recaptcha-v3';
 
 ReactDOM.render(
-  <React.StrictMode>
+  <GoogleReCaptchaProvider reCaptchaKey="6LeIxAcTAAAAAJcZVRqyHh71UMIEGNQ_MXjiZKhI">
     <App />
-  </React.StrictMode>,
+  </GoogleReCaptchaProvider>,
   document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
